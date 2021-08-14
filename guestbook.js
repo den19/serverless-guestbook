fcf0e22e0d9ff81038e3eb51624b321d757b798d
den19 <den.kolesov@gmail.com>
2021-08-15 00:52:59 +0300

    Changed apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://92e83312.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
